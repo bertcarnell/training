ad971642b050e68ed8df67ea6178b3d50ea3967f
bercarnell <bertcarnell@gmail.com>
2018-09-24 20:53:07 -0400

    updates to HW

--- neural_networks_for_ml_u_toronto/Assignment3/a3.m
+++ neural_networks_for_ml_u_toronto/Assignment3/a3.m
@@ -130,6 +130,11 @@ function ret = loss(model, data, wd_coefficient)
   log_class_prob = class_input - repmat(class_normalizer, [size(class_input, 1), 1]); % log of probability of each class. size: <number of classes, i.e. 10> by <number of data cases>
   class_prob = exp(log_class_prob); % probability of each class. Each column (i.e. each case) sums to 1. size: <number of classes, i.e. 10> by <number of data cases>
   
+  % size(log_class_prob) c x N
+  % size(data.targets) c x N
+  % size(sum(log_class_prob .* data.targets, 1)) 1 x N
+  % size(mean(sum(log_class_prob .* data.targets, 1))) 1 x 1
+  
   classification_loss = -mean(sum(log_class_prob .* data.targets, 1)); % select the right log class probability using that sum; then take the mean over all data cases.
   wd_loss = sum(model_to_theta(model).^2)/2*wd_coefficient; % weight decay loss. very straightforward: E = 1/2 * wd_coeffecient * theta^2
   ret = classification_loss + wd_loss;
@@ -144,8 +149,40 @@ function ret = d_loss_by_d_model(model, data, wd_coefficient)
   % The returned object is supposed to be exactly like parameter <model>, i.e. it has fields ret.input_to_hid and ret.hid_to_class. However, the contents of those matrices are gradients (d loss by d model parameter), instead of model parameters.
 	 
   % This is the only function that you're expected to change. Right now, it just returns a lot of zeros, which is obviously not the correct output. Your job is to replace that by a correct computation.
-  ret.input_to_hid = model.input_to_hid * 0;
-  ret.hid_to_class = model.hid_to_class * 0;
+  % ret.input_to_hid = model.input_to_hid * 0;
+  % ret.hid_to_class = model.hid_to_class * 0;
+  
+  % m = <number of hidden units>
+  m = size(model.input_to_hid, 1);
+  % N = <number of data cases>
+  N = size(data.inputs, 2);
+  % c = number of classes
+  c = size(model.hid_to_class, 1);
+  % p = number of inputs
+  p = size(model.input_to_hid, 2);
+  wd_loss_gradient = model_to_theta(model) * wd_coefficient; % (m*p + m*c) x 1
+  
+  % use internals from the loss function
+  hid_input = model.input_to_hid * data.inputs; % m x N = m x p * p x N
+  hid_output = logistic(hid_input); % m x N
+  class_input = model.hid_to_class * hid_output; % c x N = c x m * m x N
+  class_normalizer = log_sum_exp_over_rows(class_input); % 1 x N
+  log_class_prob = class_input - repmat(class_normalizer, [size(class_input, 1), 1]);  % c x N
+  class_prob = exp(log_class_prob); % c x N
+  
+  % m x N = (m x N * m x N)
+  temp_partial = hid_output .* (1-hid_output);
+  
+  % m x p = m x c * (c x N - c x N) .* m x N * N x p
+  classification_loss_gradient_1 = transpose(model.hid_to_class) * (class_prob - data.targets) .* temp_partial * transpose(data.inputs) / N;
+
+  % c x m = (c x N - c x N) * N x m
+  classification_loss_gradient_2 = (class_prob - data.targets) * transpose(hid_output) / N;
+  
+  classification_loss_gradient_1_transpose = transpose(classification_loss_gradient_1);
+  classification_loss_gradient_2_transpose = transpose(classification_loss_gradient_2);
+  classification_loss_gradient = [classification_loss_gradient_1_transpose(:); classification_loss_gradient_2_transpose(:)];
+  ret = theta_to_model(wd_loss_gradient + classification_loss_gradient);
 end
 
 function ret = model_to_theta(model)
@@ -178,3 +215,4 @@ function ret = classification_performance(model, data)
   [dump, targets] = max(data.targets); % targets is integer: the target class, plus 1.
   ret = mean(double(choices ~= targets));
 end
+
